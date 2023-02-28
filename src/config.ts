bfbd3196a721bfe36df57579c85d60d61e00c668
Sasikanth Miriyampalli <sasikanth@obvious.in>
2023-02-28 22:51:36 +0530

    Transform Figma tokens to Style Dictionary tokens programatically

--- src/config.ts
+++ src/config.ts
@@ -9,51 +9,53 @@ import {
   iOSThemeProtocolFormatter,
 } from './formatters/ios_formatters';
 
-export const config: Config = {
-  source: ['tokens/sd_tokens.json'],
-  format: {
-    androidThemeFormat: androidThemeFormat,
-    androidThemeAttrsFormat: androidThemeAttrsFormat,
-    iOSBaseColorsFormatter: iOSBaseColorsFormatter,
-    iOSThemeColorsProtocolFormatter: iOSThemeColorsProtocolFormatter,
-    iOSThemeProtocolFormatter: iOSThemeProtocolFormatter,
-  },
-  platforms: {
-    android: {
-      transformGroup: 'android',
-      buildPath: 'output/android/',
-      files: [
-        {
-          destination: 'res/colors.xml',
-          format: 'android/colors',
-        },
-        {
-          destination: 'res/base_theme.xml',
-          format: 'androidThemeFormat',
-        },
-        {
-          destination: 'res/theme_attrs.xml',
-          format: 'androidThemeAttrsFormat',
-        },
-      ],
+export function config(tokensPath: String): Config {
+  return {
+    source: [tokensPath.toString()],
+    format: {
+      androidThemeFormat: androidThemeFormat,
+      androidThemeAttrsFormat: androidThemeAttrsFormat,
+      iOSBaseColorsFormatter: iOSBaseColorsFormatter,
+      iOSThemeColorsProtocolFormatter: iOSThemeColorsProtocolFormatter,
+      iOSThemeProtocolFormatter: iOSThemeProtocolFormatter,
     },
-    ios: {
-      transforms: ['name/ti/camel', 'color/hex'],
-      buildPath: 'output/ios/',
-      files: [
-        {
-          destination: 'BaseColor.swift',
-          format: 'iOSBaseColorsFormatter',
-        },
-        {
-          destination: 'ThemeColors.swift',
-          format: 'iOSThemeColorsProtocolFormatter',
-        },
-        {
-          destination: 'Theme.swift',
-          format: 'iOSThemeProtocolFormatter',
-        },
-      ],
+    platforms: {
+      android: {
+        transformGroup: 'android',
+        buildPath: 'output/android/',
+        files: [
+          {
+            destination: 'res/colors.xml',
+            format: 'android/colors',
+          },
+          {
+            destination: 'res/base_theme.xml',
+            format: 'androidThemeFormat',
+          },
+          {
+            destination: 'res/theme_attrs.xml',
+            format: 'androidThemeAttrsFormat',
+          },
+        ],
+      },
+      ios: {
+        transforms: ['name/ti/camel', 'color/hex'],
+        buildPath: 'output/ios/',
+        files: [
+          {
+            destination: 'BaseColor.swift',
+            format: 'iOSBaseColorsFormatter',
+          },
+          {
+            destination: 'ThemeColors.swift',
+            format: 'iOSThemeColorsProtocolFormatter',
+          },
+          {
+            destination: 'Theme.swift',
+            format: 'iOSThemeProtocolFormatter',
+          },
+        ],
+      },
     },
-  },
-};
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
 import * as StyleDictionary from 'style-dictionary';
 import {config} from './config';
+import {mkdir, readFile, writeFile} from 'node:fs/promises';
+import * as path from 'path';
+const {transformTokens} = require('token-transformer');
 
-function run() {
+async function transformAndWriteTokens(tokensOutput: String): Promise<String> {
+  const transformerOptions = {
+    expandTypography: false,
+    expandShadow: false,
+    expandComposition: false,
+    expandBorder: false,
+    preserveRawValue: false,
+    throwErrorWhenNotResolved: false,
+    resolveReferences: false,
+  };
+
+  const tempDirectoryPath = 'build/tokens';
+  await mkdir(tempDirectoryPath, {recursive: true});
+
+  const styleDictionaryTokensPath = `${tempDirectoryPath}/sd_tokens.json`;
+  const styleDictionaryTokens = transformTokens(
+    JSON.parse(tokensOutput.toString()),
+    [],
+    [],
+    transformerOptions
+  );
+  await writeFile(
+    styleDictionaryTokensPath,
+    JSON.stringify(styleDictionaryTokens, null, 2)
+  );
+
+  return styleDictionaryTokensPath;
+}
+
+async function run() {
   try {
-    StyleDictionary.extend(config).buildAllPlatforms();
+    // Transform tokens
+    const tokensPath = path.resolve(__dirname, '../../tokens/tokens.json');
+    const tokensOutput = await readFile(tokensPath, {
+      encoding: 'utf-8',
+      flag: 'r',
+    });
+
+    const styleDictionaryTokensPath = await transformAndWriteTokens(
+      tokensOutput
+    );
+
+    // Run Style Dictionary
+    StyleDictionary.extend(
+      config(styleDictionaryTokensPath)
+    ).buildAllPlatforms();
   } catch (e) {
     console.log(e);
   }
